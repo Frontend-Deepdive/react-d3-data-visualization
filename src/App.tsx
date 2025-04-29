fca53068c99caea9106aa200991649051fa388a2
silverain <ekdldkaa@naver.com>
2025-04-29 14:32:31 +0900

    feat : axios, react-query library 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
+import CandleData from './components/CandleData';
 
 function App() {
-  return <div>this is me</div>;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <CandleData />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
